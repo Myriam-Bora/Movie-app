271fa58bd8c1165d02edd4418e260c6c82dacbbe
Myriam-Bora <dafnerina59@gmail.com>
2020-10-05 15:17:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 
+function Food({ name, heart }) {
+  return (
+    <div>
+      <h2>FOOD IS {name}</h2>
+      <h3>{heart}</h3>
+    </div>
+  );
+}
+
+const ILikeFood = [
+  { name: "kimchi", heart: "❤❤" },
+  { name: "kimbab", heart: "❤" },
+  { name: "gomtang", heart: "❤❤❤❤" },
+];
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      {ILikeFood.map((dish) => (
+        <Food name={dish.name} heart={dish.heart} />
+      ))}
+      {/* {ILikeFood.map(function (dish) {
+        return <Food name={dish.name} heart={dish.heart} />;
+      })} */}
+    </div>
+  );
 }
 
 export default App;
