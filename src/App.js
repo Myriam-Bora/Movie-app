764a0e70a454d755f75f3703aaf11f11b494878d
Myriam-Bora <dafnerina59@gmail.com>
2020-10-06 15:15:08 +0900

    #4.1 Axios api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //await : axios는 속도가 느리기 때문에 완료가 될 때까지 기다려주는 keyword
+    //단, await을 사용하기 위해 async 펑션이 필요
   };
 
   componentDidMount() {
-    console.log("componentDidMount !");
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies(); //getMovies 메소드 호출
   }
 
   render() {
-    /*this.isLoading을 계속 할 필요없이 미리 선언해서 사용한다 */
     const { isLoading } = this.state;
     return <div>{isLoading ? "Loading..." : "Ready"}</div>;
   } /*render end */
