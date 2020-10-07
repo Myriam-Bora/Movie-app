92de3227b270c3b17e6983653113a5d1e48c7ee7
Myriam-Bora <dafnerina59@gmail.com>
2020-10-07 17:32:18 +0900

    #6.3 props를 route를 통해 details로..

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./routes/Navigation";
+import Details from "./routes/Details";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" exact={true} component={About}></Route>
+      <Route path="/details" exact={true} component={Details}></Route>
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,31 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../style.css";
+import { Link } from "react-router-dom";
 
 //props의 특정값을 가져올 때는 반듯 {} 로 감싸준다 !
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img
-        className="movie__poster"
-        src={poster}
-        alt={title}
-        title={title}
-      ></img>
-      <div className="movie__data>">
-        <h4 className="movie__title">{title}</h4>
-        <span className="movie__year">{year}</span>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="movie__genre">
-              {genre}
-            </li> //index : map을 돌리면 자동으로 index가 만들어진다
-          ))}
-        </ul>
-        {/* slice : 일정 글자수에서 자르기 */}
-        {<p className="movie__summary">{summary.slice(0, 140)} ...</p>}
+    //to안에 props도 함께 보내기
+    <Link
+      to={{
+        pathname: "/details",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img
+          className="movie__poster"
+          src={poster}
+          alt={title}
+          title={title}
+        ></img>
+        <div className="movie__data>">
+          <h4 className="movie__title">{title}</h4>
+          <span className="movie__year">{year}</span>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="movie__genre">
+                {genre}
+              </li> //index : map을 돌리면 자동으로 index가 만들어진다
+            ))}
+          </ul>
+          {/* slice : 일정 글자수에서 자르기 */}
+          {<p className="movie__summary">{summary.slice(0, 140)} ...</p>}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
