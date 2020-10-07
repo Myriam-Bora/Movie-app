0e3b838ad3cc5a4e68237901575a73d032599379
Myriam-Bora <dafnerina59@gmail.com>
2020-10-07 14:03:02 +0900

    Updates

---
+++ precache-manifest.66edc74cf74e0c4925a6f3c6460495b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc3c4ac0dfd26ed1abb6c69e8f77351",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "d608fb76f93de605f1b8",
+    "url": "/Movie-app/static/css/main.b1c6908d.chunk.css"
+  },
+  {
+    "revision": "96ebc0417abb57a13576",
+    "url": "/Movie-app/static/js/2.8620ea1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-app/static/js/2.8620ea1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d608fb76f93de605f1b8",
+    "url": "/Movie-app/static/js/main.851471b8.chunk.js"
+  },
+  {
+    "revision": "7e11988b311f6639fa84",
+    "url": "/Movie-app/static/js/runtime-main.7386978d.js"
+  }
+]);
